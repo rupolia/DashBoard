c17d3b1b37b85e730fbeaa05a230742a275dbb28
Harshil Rupolia <65480465+rupolia@users.noreply.github.com>
2023-04-18 11:48:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./component/Home/Home";
+import Login from "./component/Login/Login";
+import Signup from "./component/Signup/Signup";
+import Dashboard from './component/Dashboard/Dashboard';
+import Blogs from './component/Blog/Blogs'
+import News from './component/News/News';
+import Logout from './component/LogOut/Logout'
+import { auth } from "./firebase";
+
 import './App.css';
 
 function App() {
+  const [UserName, setUserName] = useState("");
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserName(user.displayName);
+      } else setUserName("");
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+
+          <Route path='/' element={<Home/>}/>
+          <Route path='/dashboard' element={<Dashboard />}/>
+          <Route path='/blogs' element={<Blogs/>} />
+          <Route path='/news' element={<News/>}/>
+          <Route path='/logout' element={<Logout/>}/>
+          
+          
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
